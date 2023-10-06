f28fed9530e65ace74983da55457d5439d7c0f79
rohit verma <it.rohitverma@gmail.com>
2023-10-06 16:25:27 +0530

    js/notes implementation

---
+++ js/initial.js
@@ -0,0 +1,5 @@
+// Implement crud operation first to understand what to do 
+const showMeMyName = () => {
+  window.alert("Hello My name is Deepanshu");
+  window.alert("Hello My brother name is Himanshu");
+};
